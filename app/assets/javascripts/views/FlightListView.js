57db7d5ddc0d3eb63db28a64d4628c8bd06ab98c
Jessica Tran <kryx11@gmail.com>
2017-02-08 11:34:56 +1100

    listing flight details

--- app/assets/javascripts/views/FlightListView.js
+++ app/assets/javascripts/views/FlightListView.js
@@ -7,8 +7,11 @@ app.FlightListView = Backbone.View.extend({ //this is a page - index of posts
     var flightno = this.model.get('flight_no');
     var date = this.model.get('date');
     var trip = this.model.get('origin') + " > " + this.model.get('destination');
-    var plane = this.model.get('airplane_id');
-    var str = [flightno, date, trip, plane].join(' | ');
+
+    var planeID = this.model.get('airplane_id');
+    var findPlane = app.airplanes.where({ id: planeID });
+    var planeName = findPlane.get('name');
+    var str = [flightno, date, trip, planeName].join(' | ');
     this.$el.text(str);
     this.$el.appendTo('#results');
   }
